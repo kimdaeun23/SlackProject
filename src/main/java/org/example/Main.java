0a5f3c340ef82cb30c06d4053c9cf9731e027e93
nb-2020-13 <@>
2022-12-21 14:10:11 +0900

    FEAT Using Slack APi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,25 @@
 package org.example;
 
 import com.slack.api.methods.SlackApiException;
-
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws SlackApiException, IOException {
-        SlackMessage slackMessage=new SlackMessage();
-        slackMessage.setToken("xoxb-1376359396183-4544660391396-RPrF5aijviIE01GHoe1e6veM");
-        slackMessage.setChannel("C04ESJJTZ5E");
-        slackMessage.setText("Hi");
+    public static void main(String[] args) throws IOException, SlackApiException {
 
-        SlackUtils.sendSlack(slackMessage);
+        SlackMessage slackMessage=new SlackMessage();
+        //사용자토큰
+        slackMessage.setToken("xoxp-1376359396183-4020602894023-4542296825858-180debeeac5b8c6d5ab6d157ed67c1e3");
+        //채널ID
+        slackMessage.setChannel("C04FHJY8D63");
+        //메시지내용
+        slackMessage.setText(":wave: hi");
+
+        //텍스트 전송
+        SlackUtils.SendText(slackMessage);
+        //메시지 탬플릿(버튼) 전송
+        SlackUtils.SendKit(slackMessage);
+
+
+
     }
 }
--- src/main/java/org/example/SlackUtils.java
+++ src/main/java/org/example/SlackUtils.java
@@ -4,13 +4,22 @@ import com.slack.api.Slack;
 import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import com.slack.api.model.Message;
+import com.slack.api.model.block.LayoutBlock;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.slack.api.model.block.Blocks.*;
+import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
+import static com.slack.api.model.block.composition.BlockCompositions.plainText;
+import static com.slack.api.model.block.element.BlockElements.*;
 
 public class SlackUtils {
+    private static Slack slack=Slack.getInstance();
 
-    public static void sendSlack(SlackMessage slackMessage) throws SlackApiException, IOException {
-        Slack slack = Slack.getInstance();
+    //텍스트 전송
+    public static void SendText(SlackMessage slackMessage) throws SlackApiException, IOException {
 
         ChatPostMessageResponse response = slack.methods(slackMessage.getToken()).chatPostMessage(req -> req
                 .channel(slackMessage.getChannel())
@@ -23,4 +32,40 @@ public class SlackUtils {
             System.out.println("error: "+errorCode);
         }
     }
+
+    //메시지 탬플릿(버튼) 전송
+    public static void SendKit(SlackMessage slackMessage) throws IOException, SlackApiException {
+
+        List<LayoutBlock> layoutBlocks = new ArrayList<>();
+        layoutBlocks.add(section(section -> section.text(markdownText("메시지 탬플릿"))));
+        layoutBlocks.add(divider());
+        layoutBlocks.add(
+                actions(actions -> actions
+                        .elements(asElements(
+                                button(b -> b.text(plainText(pt -> pt.emoji(true).text("승인")))
+                                        .value("v1")
+                                        .style("primary")
+                                        .actionId("action_approve")
+                                ),
+                                button(b -> b.text(plainText(pt -> pt.emoji(true).text("거부")))
+                                        .value("v2")
+                                        .style("danger")
+                                        .actionId("action_reject")
+                                )
+
+                        ))
+                )
+        );
+        ChatPostMessageResponse response = slack.methods(slackMessage.getToken()).chatPostMessage(req -> req
+                .channel(slackMessage.getChannel())
+                        .blocks(layoutBlocks));
+        if (response.isOk()) {
+            Message postedMessage = response.getMessage();
+            System.out.println("success: "+postedMessage);
+        } else {
+            String errorCode = response.getError();
+            System.out.println("error: "+errorCode);
+        }
+    }
+
 }
